1a11682af67c301fd9ebdb399852f9f2213bc3a2
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-17 01:23:05 +0100

    init zap

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
+const zap = @import("zap");
+
+fn on_request(request: zap.Request) void {
+    request.sendBody("Hello from ZAP") catch return;
+}
 
 pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+    var listener = zap.HttpListener.init(.{
+        .port = 3000,
+        .on_request = on_request,
+        .log = true,
+    });
+    try listener.listen();
+
+    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
 }
