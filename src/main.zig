5cc5936a45939f7b8dfe305a8c70d001cf416c67
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-23 20:10:14 +0100

    parse json from post endpoint

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const zap = @import("zap");
+const MovieModel = @import("models/movie.zig");
+const Movie = MovieModel.Movie;
 
 fn on_request(request: zap.Request) void {
     if (request.path != null) {
@@ -9,13 +11,20 @@ fn on_request(request: zap.Request) void {
                 request.sendBody("Get all movies") catch return;
             }
 
-            if (request.methodAsEnum() == .POST) {
-                std.debug.print("Post movie id=, title='' and relase_date=\n", .{});
-                request.setStatus(.ok);
-                return;
+            if (request.methodAsEnum() == .POST) {           
+                if (request.body) |body| {
+                    const movie_opt: ?std.json.Parsed(Movie) = std.json.parseFromSlice(Movie, std.heap.page_allocator, body, .{}) catch null;
+                    if (movie_opt) |movie| {
+                        std.debug.print("validate\n", .{});
+                        defer movie.deinit();
+                        std.debug.print("Post movie id={d}, title='{s}' and relase_date={s}\n", .{ movie.value.id, movie.value.title, movie.value.release_date });
+                        request.setStatus(.ok);
+                        return;
+                    }
+                }
             }
         }
-        
+
         if (std.mem.startsWith(u8, request.path.?, "/api/movies/")) {
             if(request.methodAsEnum() == .DELETE) {
                 std.debug.print("Delete movie with id \n", .{});
